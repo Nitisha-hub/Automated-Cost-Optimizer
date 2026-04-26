30348d378bc65a8c9c69391ba7ff9fede6213dab
Nitisha <nitishamali23@gmail.com>
2026-04-26 14:38:26 +0530

    Add lambda function to stop a specific EC2 instance
    This function checks the state of EC2 instances and stops a specific running instance if it matches the target ID.

--- lambda/cost_optimizer.py
+++ lambda/cost_optimizer.py
@@ -1,1 +1,24 @@
 
+import boto3
+
+ec2 = boto3.client('ec2')
+
+TARGET_INSTANCE_ID = "i-053037aa05547dbe8"
+
+def lambda_handler(event, context):
+    response = ec2.describe_instances()
+    stopped_instances = []
+
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            state = instance['State']['Name']
+
+            if instance_id == TARGET_INSTANCE_ID and state == "running":
+                ec2.stop_instances(InstanceIds=[instance_id])
+                stopped_instances.append(instance_id)
+
+    return {
+        "status": "success",
+        "stopped_instances": stopped_instances
+    }
