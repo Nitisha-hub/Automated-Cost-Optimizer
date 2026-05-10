0b57d04b4088bb85ae49e5160c90d9028a2dc1f8
Nitisha <nitishamali23@gmail.com>
2026-05-10 23:24:37 +0530

    Update cost_optimizer.py

--- lambda/cost_optimizer.py
+++ lambda/cost_optimizer.py
@@ -3,7 +3,7 @@ import boto3
 
 ec2 = boto3.client('ec2')       
 
-TARGET_INSTANCE_ID = "i-053037aa05547dbe8"
+TARGET_INSTANCE_ID = "i-053037aa05547dbe8"                    
 
 def lambda_handler(event, context):
     response = ec2.describe_instances()
